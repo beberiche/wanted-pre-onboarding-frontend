ccae611b32e854403276752f7cdcbc7cd0caadf3
beberiche <woaol@naver.com>
2023-06-11 00:13:13 +0900

    ✨ feat(all): eslint&prettier 설정(airbnb)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>CRA, Typescript, yarn</h2>
+      <h2>CRA, Typescript, yarn </h2>
     </div>
   );
 }
