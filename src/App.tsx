144bb9dec7d3a4c33255513746b10fcfb11b4edb
beberiche <woaol@naver.com>
2023-06-10 19:23:21 +0900

    ✨ feat(all): 패키지 매니저 npm -> yarn 으로 변경, CRA, Typescript, 테스팅 라이브러리 삭제 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello React with Typescript</h2>
+      <h2>CRA, Typescript, yarn</h2>
     </div>
   );
 }
